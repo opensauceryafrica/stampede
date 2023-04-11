e1928c8c24285ea430647044411fcb55aed4f826
Open Saucerer <lovedayperfection1@gmail.com>
2023-04-11 05:22:50 +0100

    chore: global constants

--- constant.ts
+++ constant.ts
@@ -1,7 +1,34 @@
+/**
+ * URLS used in the application
+ */
 const URLs = {
-  FileBin: 'https://filebin.net/stampede/',
+  FileBin: 'https://filebin.net/',
+  PDFJSWorker:
+    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js',
+  PDFJSLib: 'pdfjs-dist/build/pdf',
+  GoFile: 'https://{}.gofile.io',
 };
 
+/**
+ * Misc constants used in the application
+ */
 const Misc = {
-  FileBin: 'stampede',
+  Bin: 'stampedee',
+  Danger: 'danger',
+  Success: 'success',
 };
+
+interface Context {
+  PDF?: string;
+  Stamp?: string;
+  PDFName?: string;
+  StampName?: string;
+  CollectorLoad?: number;
+  Folder?: string;
+  Server?: string;
+}
+
+/**
+ * Context used in the application
+ */
+let Context: Context = {};
